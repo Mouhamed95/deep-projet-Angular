451d4fd717b507cb12e94b7c2175cea5858bcf9d
Mouhamed95 <lomoustapha95@gmail.com>
2025-04-21 21:54:30 +0000

    ng-content and slots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ServerComponent } from './server/server.component';
+import { TrafficComponent } from './traffic/traffic.component';
+import { SupportTicketsComponent } from './support-tickets/support-tickets.component';
+import { DashboardItemComponent } from "./dashboard-item/dashboard-item.component";
+
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    ServerComponent,
+    TrafficComponent,
+    SupportTicketsComponent,
+    DashboardItemComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'deep-projet-angular';
+ 
+  
 }
